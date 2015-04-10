015b8aee44ebcf1ae18ff3dd8462098d3a10e070
Nick R. Katsipoulakis <katsip@cs.pitt.edu>
2015-04-10 15:37:15 -0400

    Performed some additional tests on the join operator.

--- src/main/java/JoinOpTest.java
+++ src/main/java/JoinOpTest.java
@@ -10,24 +10,24 @@ import gr.katsip.synefo.storm.operators.relational.StringComparator;
 public class JoinOpTest {
 
 	public static void main(String[] args) {
-		String[] leftSchema = { "one", "two" };
-		String[] rightSchema = {"three", "two" };
+		String[] leftSchema = { "num", "one", "two", "three", "four" };
+		String[] rightSchema = { "num", "1", "2", "three", "5" };
 		Fields lf = new Fields(leftSchema);
 		Fields rf = new Fields(rightSchema);
-		JoinOperator joinOperator = 
-				new JoinOperator(new StringComparator(), 100, "two", new Fields(leftSchema), new Fields(rightSchema));
+		JoinOperator<String> joinOperator = 
+				new JoinOperator<String>(new StringComparator(), 100, "three", new Fields(leftSchema), new Fields(rightSchema));
 		List<Values> stateValues = new ArrayList<Values>();
 		joinOperator.init(stateValues);
 		System.out.println("State schema: " + joinOperator.getStateSchema());
 		System.out.println("Output schema: " + joinOperator.getOutputSchema());
 		
-//		Values leftTuple = new Values("1", "2");
-//		List<Values> result = joinOperator.execute(lf, leftTuple);
-//		System.out.println("result: " + result.toString());
-//		result = joinOperator.getStateValues();
-//		System.out.println("state: " + stateValues.toString());
-		Values rightTuple = new Values("3", "2");
-		List<Values> result = joinOperator.execute(rf, rightTuple);
+		Values leftTuple = new Values("1", "one", "two", "three", "four");
+		List<Values> result = joinOperator.execute(lf, leftTuple);
+		System.out.println("result: " + result.toString());
+		result = joinOperator.getStateValues();
+		System.out.println("state: " + stateValues.toString());
+		Values rightTuple = new Values("2", "1", "2", "three", "5");
+		result = joinOperator.execute(rf, rightTuple);
 		System.out.println("result: " + result.toString());
 		result = joinOperator.getStateValues();
 		System.out.println("state: " + result.toString());
