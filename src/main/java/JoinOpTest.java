cd98503057055c78636e7d02042e09274a40a936
Nick R. Katsipoulakis <nick.katsip@gmail.com>
2015-04-17 10:58:54 -0400

    Minor change + change on the thresholds

--- src/main/java/JoinOpTest.java
+++ src/main/java/JoinOpTest.java
@@ -10,30 +10,30 @@ import gr.katsip.synefo.storm.operators.relational.StringComparator;
 public class JoinOpTest {
 
 	public static void main(String[] args) {
-//		String[] leftSchema = { "num", "one", "two", "three", "four" };
-//		String[] rightSchema = { "num", "1", "2", "three", "5" };
-//		Fields lf = new Fields(leftSchema);
-//		Fields rf = new Fields(rightSchema);
-//		JoinOperator<String> joinOperator = 
-//				new JoinOperator<String>(new StringComparator(), 100, "three", new Fields(leftSchema), new Fields(rightSchema));
-//		List<Values> stateValues = new ArrayList<Values>();
-//		joinOperator.init(stateValues);
-//		System.out.println("State schema: " + joinOperator.getStateSchema());
-//		System.out.println("Output schema: " + joinOperator.getOutputSchema());
-//		
-//		Values leftTuple = new Values("1", "one", "two", "three", "four");
-//		List<Values> result = joinOperator.execute(lf, leftTuple);
-//		System.out.println("result: " + result.toString());
-//		result = joinOperator.getStateValues();
-//		System.out.println("state: " + stateValues.toString());
-//		Values rightTuple = new Values("2", "1", "2", "three", "5");
-//		result = joinOperator.execute(rf, rightTuple);
-//		System.out.println("result: " + result.toString());
-//		result = joinOperator.getStateValues();
-//		System.out.println("state: " + result.toString());
-//		joinOperator.mergeState(new Fields(), result);
-//		result = joinOperator.getStateValues();
-//		System.out.println("state: " + result.toString());
+		String[] leftSchema = { "num", "one", "two", "three", "four" };
+		String[] rightSchema = { "num", "1", "2", "three", "5" };
+		Fields lf = new Fields(leftSchema);
+		Fields rf = new Fields(rightSchema);
+		JoinOperator<String> joinOperator = 
+				new JoinOperator<String>(new StringComparator(), 100, "three", new Fields(leftSchema), new Fields(rightSchema));
+		List<Values> stateValues = new ArrayList<Values>();
+		joinOperator.init(stateValues);
+		System.out.println("State schema: " + joinOperator.getStateSchema());
+		System.out.println("Output schema: " + joinOperator.getOutputSchema());
+		
+		Values leftTuple = new Values("1", "one", "two", "three", "four");
+		List<Values> result = joinOperator.execute(lf, leftTuple);
+		System.out.println("result: " + result.toString());
+		result = joinOperator.getStateValues();
+		System.out.println("state: " + stateValues.toString());
+		Values rightTuple = new Values("2", "1", "2", "three", "5");
+		result = joinOperator.execute(rf, rightTuple);
+		System.out.println("result: " + result.toString());
+		result = joinOperator.getStateValues();
+		System.out.println("state: " + result.toString());
+		joinOperator.mergeState(new Fields(), result);
+		result = joinOperator.getStateValues();
+		System.out.println("state: " + result.toString());
 		String task = "ADD~bolt_1a:12@123.421.123.421";
 		String[] tokens = task.split("[~:@]");
 		for(String t : tokens) {
