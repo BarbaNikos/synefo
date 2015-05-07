ecc6acf68e1344bfe6fd4cf910cc84602a5f4e28
Nick R. Katsipoulakis <katsip@cs.pitt.edu>
2015-05-07 16:45:10 -0400

    Removed some bugs from the cleaning directories procedure.

--- src/main/java/gr/katsip/synefo/storm/topology/crypefo/DemoTopologyOne.java
+++ src/main/java/gr/katsip/synefo/storm/topology/crypefo/DemoTopologyOne.java
@@ -94,19 +94,25 @@ public class DemoTopologyOne {
 		try {
 			ZooKeeper zk = new ZooKeeper(zooIP + ":" + zooPort, 100000, null);
 			if(zk.exists("/data", false) != null) {
+				System.out.println("Z-Node \"/data\" exists so we need to clean it up...");
 				List<String> operators = zk.getChildren("/data", false);
 				if(operators != null && operators.size() > 0) {
+					System.out.println("Located (" + operators.size() + ") of Z-Node \"/data\" that need to be removed.");
 					for(String operator : operators) {
 						List<String> dataPoints = zk.getChildren("/data/" + operator, false);
 						if(dataPoints != null && dataPoints.size() > 0) {
+							System.out.println("Located " + dataPoints.size() + " data points for operator " + operator + ". removing them sequentially...");
 							for(String dataPoint : dataPoints) {
 								zk.delete("/data/" + operator + "/" + dataPoint, -1);
 							}
+							System.out.println("..." + dataPoints.size() + " data points removed.");
 						}
 						zk.delete("/data/" + operator, -1);
+						System.out.println("... operator removed.");
 					}
 				}
 				zk.delete("/data", -1);
+				System.out.println(".. \"/data\" z-node removed.");
 			}
 			zk.create("/data", ("/data").getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 			zk.close();
