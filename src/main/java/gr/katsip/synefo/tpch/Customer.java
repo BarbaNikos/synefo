b473c0cedd713665994d43911c62c6b9229b1af4
Nick R. Katsipoulakis <nick.katsip@gmail.com>
2015-06-14 20:10:54 -0400

    Started working on the TPC-H Query 5.

---
+++ src/main/java/gr/katsip/synefo/tpch/Customer.java
@@ -0,0 +1,25 @@
+package gr.katsip.synefo.tpch;
+
+public class Customer {
+	
+	/**
+	 * C_CUSTKEY BIGINT
+	 * C_NAME VARCHAR(25)
+	 * C_ADDRESS VARCHAR(40)
+	 * C_NATIONKEY INT
+	 * C_PHONE VARCHAR(15)
+	 * C_ACCTBAL DECIMAL
+	 * C_COMMENT VARCHAR(117)
+	 */
+	public static final String[] schema = { 
+			"C_CUSTKEY", 
+			"C_NAME", 
+			"C_ADDRESS", 
+			"C_NATIONKEY", 
+			"C_PHONE", 
+			"C_ACCTBAL", 
+			"C_MKTSEGMENT", 
+			"C_COMMENT" 
+			};
+	
+}
