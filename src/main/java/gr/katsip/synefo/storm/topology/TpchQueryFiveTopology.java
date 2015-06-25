674f6c2c971be5e791602634603571dc48966019
Nick R. Katsipoulakis <katsip@cs.pitt.edu>
2015-06-25 11:28:55 -0400

    Set the number of workers equal to the number of available workers in the cluster.

--- src/main/java/gr/katsip/synefo/storm/topology/TpchQueryFiveTopology.java
+++ src/main/java/gr/katsip/synefo/storm/topology/TpchQueryFiveTopology.java
@@ -191,7 +191,7 @@ public class TpchQueryFiveTopology {
 		synEFOSocket.close();
 
 		conf.setDebug(false);
-//		conf.setNumWorkers(8);
+		conf.setNumWorkers(16);
 		conf.put(Config.TOPOLOGY_WORKER_CHILDOPTS, "-Xmx4096m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:NewSize=128m -XX:CMSInitiatingOccupancyFraction=70 -XX:-CMSConcurrentMTEnabled -Djava.net.preferIPv4Stack=true");
 		conf.put(Config.TOPOLOGY_RECEIVER_BUFFER_SIZE, 8);
 		conf.put(Config.TOPOLOGY_TRANSFER_BUFFER_SIZE, 32);
