7cb7dabb1c1dfb63da9bbc7bb8dc5c8cf9588d89
Nick R. Katsipoulakis <katsip@cs.pitt.edu>
2015-09-08 15:41:20 -0400

    Set the number of ackers equal to number of workers.

--- src/main/java/gr/katsip/synefo/storm/topology/TpchQueryFiveTopology.java
+++ src/main/java/gr/katsip/synefo/storm/topology/TpchQueryFiveTopology.java
@@ -246,6 +246,7 @@ public class TpchQueryFiveTopology {
                 "-Xmx8192m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:NewSize=128m -XX:CMSInitiatingOccupancyFraction=70 -XX:-CMSConcurrentMTEnabled -Djava.net.preferIPv4Stack=true");
 		conf.put(Config.TOPOLOGY_RECEIVER_BUFFER_SIZE, 8);
         conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, maxSpoutPending);
+        conf.setNumAckers(workerNum);
 		conf.put(Config.TOPOLOGY_TRANSFER_BUFFER_SIZE, 32);
 		conf.put(Config.TOPOLOGY_EXECUTOR_RECEIVE_BUFFER_SIZE, 16384);
 		conf.put(Config.TOPOLOGY_EXECUTOR_SEND_BUFFER_SIZE, 16384);
