2b7123d9ef92927d429ad804b687b1c3ec884f3e
Nick R. Katsipoulakis <katsip@cs.pitt.edu>
2015-09-24 13:29:14 -0400

    Changed a small error in threshold parsing.

--- src/main/java/gr/katsip/synefo/balancer/App.java
+++ src/main/java/gr/katsip/synefo/balancer/App.java
@@ -14,15 +14,16 @@ public class App {
             System.err.println("arguments: <resource-file-thresholds.xml> <zoo-ip1:port1,zoo-ip2:port2,...,zoo-ipN:portN>");
             System.exit(1);
         }
-        ResourceThresholdParser parser = new ResourceThresholdParser();
-        parser.parseThresholds(args[0]);
+//        ResourceThresholdParser parser = new ResourceThresholdParser();
+//        parser.parseThresholds(args[0]);
         String zookeeperAddress = args[1];
-        HashMap<String, Pair<String, String>> inputRateThresholds = new HashMap<>();
-        Pair<String, String> pair = new Pair<>();
-        pair.lowerBound = "1000";
-        pair.upperBound = "1000";
+        HashMap<String, Pair<Number, Number>> inputRateThresholds = new HashMap<>();
+        Pair<Number, Number> pair = new Pair<>();
+        pair.lowerBound = new Integer(1000);
+        pair.upperBound = new Integer(1000);
         inputRateThresholds.put("input-rate", pair);
-        BalanceServer server = new BalanceServer(zookeeperAddress, parser.get_thresholds(), null);
+//        BalanceServer server = new BalanceServer(zookeeperAddress, parser.get_thresholds(), null);
+        BalanceServer server = new BalanceServer(zookeeperAddress, inputRateThresholds, null);
         server.runServer();
     }
 }
