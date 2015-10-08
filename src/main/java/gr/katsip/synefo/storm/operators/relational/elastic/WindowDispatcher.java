cc7e68fa431e6ffcf15bd324be37b992a03ae454
Nick R. Katsipoulakis <nick.katsip@gmail.com>
2015-10-08 18:40:09 -0400

    Continued the implementation

--- src/main/java/gr/katsip/synefo/storm/operators/relational/elastic/WindowDispatcher.java
+++ src/main/java/gr/katsip/synefo/storm/operators/relational/elastic/WindowDispatcher.java
@@ -4,23 +4,101 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by katsip on 10/8/2015.
  */
 public class WindowDispatcher implements Serializable, Dispatcher {
+
+    Logger logger = LoggerFactory.getLogger(WindowDispatcher.class);
+
+    private String outerRelationName;
+
+    private String innerRelationName;
+
+    private Fields outerRelationSchema;
+
+    private Fields innerRelationSchema;
+
+    private String outerRelationKey;
+
+    private String innerRelationKey;
+
+    private String outerRelationForeignKey;
+
+    private String innerRelationForeignKey;
+
+    private Fields outputSchema;
+
+    private long stateSize = 0L;
+
+    private HashMap<String, List<Integer>> taskToRelationIndex;
+
+    public class DispatchWindow implements Serializable {
+
+        public long start;
+
+        public long end;
+
+        public long stateSize = 0L;
+
+        private HashMap<String, List<Integer>> outerRelationIndex;
+
+        private HashMap<String, List<Integer>> innerRelationIndex;
+
+        public DispatchWindow() {
+            stateSize = 0L;
+            start = 0L;
+            end = 0L;
+            outerRelationIndex = new HashMap<>();
+            innerRelationIndex = new HashMap<>();
+        }
+    }
+
+    private long window;
+
+    private long slide;
+
+    private List<DispatchWindow> circularCache;
+
+    private int cacheSize;
+
+    public WindowDispatcher(String outerRelationName, Fields outerRelationSchema,
+                            String outerRelationKey, String outerRelationForeignKey,
+                            String innerRelationName, Fields innerRelationSchema,
+                            String innerRelationKey, String innerRelationForeignKey, Fields outputSchema,
+                            long window, long slide) {
+        this.outerRelationName = outerRelationName;
+        this.outerRelationSchema = new Fields(outerRelationSchema.toList());
+        this.outerRelationKey = outerRelationKey;
+        this.outerRelationForeignKey = outerRelationForeignKey;
+        this.innerRelationName = innerRelationName;
+        this.innerRelationSchema = new Fields(innerRelationSchema.toList());
+        this.innerRelationKey = innerRelationKey;
+        this.innerRelationForeignKey = innerRelationForeignKey;
+        taskToRelationIndex = null;
+        this.outputSchema = new Fields(outputSchema.toList());
+        circularCache = new LinkedList<>();
+        this.window = window;
+        this.slide = slide;
+        cacheSize = (int) Math.ceil(window / slide);
+    }
+
     @Override
     public void setTaskToRelationIndex(HashMap<String, List<Integer>> taskToRelationIndex) {
-
+        this.taskToRelationIndex = new HashMap<>(taskToRelationIndex);
     }
 
     @Override
     public void setOutputSchema(Fields outputSchema) {
-
+        this.outputSchema = new Fields(outputSchema.toList());
     }
 
     @Override
@@ -30,7 +108,7 @@ public class WindowDispatcher implements Serializable, Dispatcher {
 
     @Override
     public Fields getOutputSchema() {
-        return null;
+        return outputSchema;
     }
 
     @Override
