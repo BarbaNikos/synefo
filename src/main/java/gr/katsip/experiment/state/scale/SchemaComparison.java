c05a6b5f9fff26000f88a2a3e18036bf551e56e6
Nick R. Katsipoulakis <katsip@cs.pitt.edu>
2015-10-20 18:21:50 -0400

    Found out that this is not the problem with Schema-comparison.

--- src/main/java/gr/katsip/experiment/state/scale/SchemaComparison.java
+++ src/main/java/gr/katsip/experiment/state/scale/SchemaComparison.java
@@ -2,6 +2,9 @@ package gr.katsip.experiment.state.scale;
 
 import backtype.storm.tuple.Fields;
 import gr.katsip.synefo.tpch.LineItem;
+import gr.katsip.synefo.tpch.Order;
+
+import java.util.Arrays;
 
 /**
  * Created by katsip on 10/20/2015.
@@ -10,10 +13,11 @@ public class SchemaComparison {
     public static void main(String args[]) {
         Fields schema = new Fields(LineItem.schema);
         Fields anotherSchema = new Fields(LineItem.schema);
+        Fields orderSchema = new Fields(Order.schema);
         System.out.println("schema: " + schema.toList().toString());
         System.out.println("another schema: " + anotherSchema.toString());
         System.out.println("are they equal: " + (schema.toList().toString().equals(anotherSchema.toList().toString())));
-
-        System.out.println(schema.toList().toArray());
+        System.out.println(Arrays.equals(schema.toList().toArray(), anotherSchema.toList().toArray()));
+        System.out.println(Arrays.equals(schema.toList().toArray(), orderSchema.toList().toArray()));
     }
 }
