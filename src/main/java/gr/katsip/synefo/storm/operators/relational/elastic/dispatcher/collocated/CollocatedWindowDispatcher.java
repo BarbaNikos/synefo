f1ca9ff10a7f14dfcbd5bcaf86ab2f2940aa91e3
Nick R. Katsipoulakis <katsip@cs.pitt.edu>
2015-10-26 14:16:28 -0400

    Updated CollocatedWindowDispatcher to not use hash functions and use indices for routing tuples.

--- src/main/java/gr/katsip/synefo/storm/operators/relational/elastic/dispatcher/collocated/CollocatedWindowDispatcher.java
+++ src/main/java/gr/katsip/synefo/storm/operators/relational/elastic/dispatcher/collocated/CollocatedWindowDispatcher.java
@@ -4,10 +4,8 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
-import gr.katsip.synefo.storm.operators.relational.elastic.dispatcher.Dispatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -49,6 +47,8 @@ public class CollocatedWindowDispatcher implements Serializable {
 
     private int outerRelationCardinality;
 
+    private int index;
+
     public CollocatedWindowDispatcher(String outerRelationName, Fields outerRelationSchema, String outerRelationKey,
                                       String innerRelationName, Fields innerRelationSchema, String innerRelationKey,
                                       Fields outputSchema, long window, long slide) {
@@ -65,84 +65,156 @@ public class CollocatedWindowDispatcher implements Serializable {
         this.outputSchema = new Fields(outputSchema.toList());
         innerRelationCardinality = 0;
         outerRelationCardinality = 0;
+        index = 0;
     }
 
     public void setTaskToRelationIndex(List<Integer> activeDownstreamTaskIdentifiers) {
-        this.taskToRelationIndex = new HashMap<>();
-        this.taskToRelationIndex.put(innerRelationName, new ArrayList<>(activeDownstreamTaskIdentifiers));
-        this.taskToRelationIndex.put(outerRelationName, new ArrayList<>(activeDownstreamTaskIdentifiers));
+        taskToRelationIndex = new HashMap<>();
+        taskToRelationIndex.put(innerRelationName, new ArrayList<>(activeDownstreamTaskIdentifiers));
+        taskToRelationIndex.put(outerRelationName, new ArrayList<>(activeDownstreamTaskIdentifiers));
+        index = 0;
     }
 
     public void setOutputSchema(Fields outputSchema) {
         this.outputSchema = new Fields(outputSchema.toList());
     }
 
+    public int locateTask(Long timestamp, String relation, String key) {
+        int task = -1;
+        for (int i = 0; i < ringBuffer.size(); i++) {
+            CollocatedDispatchWindow window = ringBuffer.get(i);
+            if ((window.start + this.window) >= timestamp) {
+                if (relation.equals(innerRelationName)) {
+                    if (window.innerRelationIndex.containsKey(key)) {
+                        task = window.innerRelationIndex.get(key).get(0);
+                        break;
+                    }
+                } else if (relation.equals(outerRelationName)) {
+                    if (window.outerRelationIndex.containsKey(key)) {
+                        task = window.outerRelationIndex.get(key).get(0);
+                        break;
+                    }
+                }
+            }else {
+                break;
+            }
+        }
+        return task;
+    }
+
     public int execute(Tuple anchor, OutputCollector collector, Fields fields, Values values) {
         long currentTimestamp = System.currentTimeMillis();
         int numberOfDispatchedTuples = 0;
+        int victimTask = -1;
+        String key = null;
         Values tuple = new Values();
         tuple.add("0");
         tuple.add(fields);
         tuple.add(values);
         if (fields.toList().toString().equals(innerRelationSchema.toList().toString())) {
-            String key = (String) values.get(innerRelationSchema.fieldIndex(innerRelationKey));
-            int victimTaskIndex = key.hashCode() % taskToRelationIndex.get(innerRelationName).size();
-            Integer victimTask = taskToRelationIndex.get(innerRelationName).get(victimTaskIndex);
-            if (collector != null)
-                collector.emitDirect(victimTask, anchor, tuple);
-            updateStatistic(currentTimestamp, innerRelationName, victimTask, key);
-            numberOfDispatchedTuples++;
-            return numberOfDispatchedTuples;
+            key = (String) values.get(innerRelationSchema.fieldIndex(innerRelationKey));
+            victimTask = locateTask(currentTimestamp, innerRelationName, key);
+            if (victimTask < 0) {
+                victimTask = taskToRelationIndex.get(innerRelationName).get(index);
+                if (index == taskToRelationIndex.get(innerRelationName).size() - 1)
+                    index = 0;
+                else
+                    index++;
+            }
         }else if (fields.toList().toString().equals(outerRelationSchema.toList().toString())) {
-            String key = (String) values.get(outerRelationSchema.fieldIndex(outerRelationKey));
-            int victimTaskIndex = key.hashCode() % taskToRelationIndex.get(outerRelationName).size();
-            Integer victimTask = taskToRelationIndex.get(outerRelationName).get(victimTaskIndex);
-            if (collector != null)
-                collector.emitDirect(victimTask, anchor, tuple);
-            updateStatistic(currentTimestamp, outerRelationName, victimTask, key);
-            numberOfDispatchedTuples++;
-            return numberOfDispatchedTuples;
+            key = (String) values.get(outerRelationSchema.fieldIndex(outerRelationKey));
+            victimTask = locateTask(currentTimestamp, outerRelationName, key);
+            if (victimTask < 0) {
+                victimTask = taskToRelationIndex.get(outerRelationName).get(index);
+                if (index == taskToRelationIndex.get(outerRelationName).size() - 1)
+                    index = 0;
+                else
+                    index++;
+            }
         }
-        return 0;
+        updateCurrentWindow(currentTimestamp, outerRelationName, key, victimTask);
+        if (collector != null && victimTask >= 0) {
+            collector.emitDirect(victimTask, anchor, tuple);
+            numberOfDispatchedTuples++;
+        }
+        return numberOfDispatchedTuples;
     }
 
-    public void updateStatistic(Long timestamp, String relation, Integer victimTask, String key) {
+    public void garbageCollect(Long timestamp) {
         if (ringBuffer.size() >= bufferSize && (ringBuffer.getLast().start + this.window) <= timestamp) {
             CollocatedDispatchWindow window = ringBuffer.removeLast();
             innerRelationCardinality -= (window.innerRelationCardinality);
             outerRelationCardinality -= (window.outerRelationCardinality);
         }
+    }
+
+    public void updateCurrentWindow(Long timestamp, String relation, String key, Integer victimTask) {
+        garbageCollect(timestamp);
         if (ringBuffer.size() == 0) {
             CollocatedDispatchWindow window = new CollocatedDispatchWindow();
             window.start = timestamp;
             window.end = window.start + slide;
+            if (relation.equals(innerRelationName)) {
+                List<Integer> tasks = new ArrayList<>();
+                tasks.add(victimTask);
+                window.innerRelationIndex.put(key, tasks);
+                window.innerRelationCardinality += 1;
+                innerRelationCardinality += 1;
+            }else if (relation.equals(outerRelationName)) {
+                List<Integer> tasks = new ArrayList<>();
+                tasks.add(victimTask);
+                window.outerRelationIndex.put(key, tasks);
+                window.outerRelationCardinality += 1;
+                outerRelationCardinality += 1;
+            }
+            window.numberOfTuplesPerTask.put(victimTask, new Integer(1));
+            window.keyToTaskMapping.put(key, victimTask);
+            window.stateSize += (key.length() + 4);
             ringBuffer.addFirst(window);
         }else if (ringBuffer.getFirst().end < timestamp && ringBuffer.size() < bufferSize) {
             CollocatedDispatchWindow window = new CollocatedDispatchWindow();
             window.start = ringBuffer.getFirst().end + 1;
             window.end = window.start + slide;
+            List<Integer> tasks = new ArrayList<>();
+            tasks.add(victimTask);
+            if (relation.equals(innerRelationName)) {
+                window.innerRelationIndex.put(key, tasks);
+                window.innerRelationCardinality += 1;
+                innerRelationCardinality += 1;
+            }else if (relation.equals(outerRelationName)) {
+                window.outerRelationIndex.put(key, tasks);
+                window.outerRelationCardinality += 1;
+                outerRelationCardinality += 1;
+            }
+            window.numberOfTuplesPerTask.put(victimTask, new Integer(1));
+            window.keyToTaskMapping.put(key, victimTask);
+            window.stateSize += (key.length() + 4);
             ringBuffer.addFirst(window);
-        }
-        if (relation.equals(innerRelationName)) {
-            if (ringBuffer.getFirst().numberOfTuplesPerTask.containsKey(victimTask)) {
-                Integer counter = ringBuffer.getFirst().numberOfTuplesPerTask.get(victimTask);
-                ringBuffer.getFirst().numberOfTuplesPerTask.put(victimTask, ++counter);
-            }else {
-                ringBuffer.getFirst().numberOfTuplesPerTask.put(victimTask, new Integer(1));
+        }else {
+            List<Integer> tasks = new ArrayList<>();
+            tasks.add(victimTask);
+            if (relation.equals(innerRelationName)) {
+                if (!ringBuffer.getFirst().innerRelationIndex.containsKey(key)) {
+                    ringBuffer.getFirst().innerRelationIndex.put(key, tasks);
+                    ringBuffer.getFirst().stateSize += (key.length() + 4);
+                }
+                ringBuffer.getFirst().innerRelationCardinality += 1;
+                innerRelationCardinality += 1;
+            }else if (relation.equals(outerRelationName)) {
+                if (!ringBuffer.getFirst().outerRelationIndex.containsKey(key)) {
+                    ringBuffer.getFirst().outerRelationIndex.put(key, tasks);
+                    ringBuffer.getFirst().stateSize += (key.length() + 4);
+                }
+                ringBuffer.getFirst().outerRelationCardinality += 1;
+                outerRelationCardinality += 1;
             }
-            if (!ringBuffer.getFirst().keyToTaskMapping.containsKey(key))
-                ringBuffer.getFirst().keyToTaskMapping.put(key, new Integer(victimTask));
-            ringBuffer.getFirst().innerRelationCardinality += 1L;
-        }else if (relation.equals(outerRelationName)) {
-            if (ringBuffer.getFirst().numberOfTuplesPerTask.containsKey(victimTask)) {
-                Integer counter = ringBuffer.getFirst().numberOfTuplesPerTask.get(victimTask);
-                ringBuffer.getFirst().numberOfTuplesPerTask.put(victimTask, ++counter);
-            }else {
-                ringBuffer.getFirst().numberOfTuplesPerTask.put(victimTask, new Integer(1));
-            }
-            if (!ringBuffer.getFirst().keyToTaskMapping.containsKey(key))
-                ringBuffer.getFirst().keyToTaskMapping.put(key, new Integer(victimTask));
-            ringBuffer.getFirst().outerRelationCardinality += 1L;
+            Integer count = null;
+            if (ringBuffer.getFirst().numberOfTuplesPerTask.containsKey(victimTask))
+                count = ringBuffer.getFirst().numberOfTuplesPerTask.get(victimTask);
+            else
+                count = new Integer(0);
+            ringBuffer.getFirst().numberOfTuplesPerTask.put(victimTask, ++count);
+            ringBuffer.getFirst().keyToTaskMapping.put(key, victimTask);
         }
     }
 
