740bf8c664084769439960b163701ec9cb68ed84
Nick R. Katsipoulakis <nick.katsip@gmail.com>
2015-11-05 19:55:31 -0500

    I figured out that controlled input rate does not work properly. I need to revisit the way it works.

---
+++ src/main/java/gr/katsip/synefo/storm/producers/ControlledProducerApp.java
@@ -0,0 +1,11 @@
+package gr.katsip.synefo.storm.producers;
+
+/**
+ * Created by katsip on 11/5/2015.
+ */
+public class ControlledProducerApp {
+
+    public static void main(String[] args) {
+
+    }
+}
