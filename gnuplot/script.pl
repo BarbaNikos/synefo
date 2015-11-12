d017647703d5f246ffeed3d8d7305bf0b9ee51a4
Nick R. Katsipoulakis <nick.katsip@gmail.com>
2015-11-12 10:55:47 -0500

    Minor change to the log file cleaner script.

--- gnuplot/script.pl
+++ gnuplot/script.pl
@@ -6,17 +6,17 @@ use warnings;
 
 my @joiner_array = (5, 6);
 
-system("grep -m 1 \"[0-9]:dispatch\" metrics.log")
+system("grep -m 1 \"[0-9]:dispatch\" metrics.log");
 
-#populate the dispatcher-input-rate file
+# populate the dispatcher-input-rate file
 system("rm -f dispatcher-input-rate.dat");
 system("grep \"dispatch\\s*input-rate\" metrics.log | sed 's/^201[0-9][-][0-9]*[-][0-9]*[ \t]*[0-9]*:[0-9]*:[0-9]*,[0-9]*[ \t]*[0-9]*[ \t]*//' | sed 's/[ \t]*astro2.cs.pitt.edu:[0-9]*[ \t]*[0-9]:dispatch[ \t]*input-rate[ \t]*/\t/' > dispatcher-input-rate.dat");
 
-#populate the latency file
+# populate the latency file
 system("rm -f latency.dat");
 system("grep \"comp-latency\" metrics.log | sed 's/^201[0-9][-][0-9]*[-][0-9]*[ \t]*[0-9]*:[0-9]*:[0-9]*,[0-9]*[ \t]*[0-9]*[ \t]*//' | sed 's/[ \t]*astro2.cs.pitt.edu:[0-9]*//' | sed 's/:[a-z]*[ \t]*comp-latency[ \t]*/\t/' > latency.dat");
 
-#populate the state size files
+# populate the state size files
 foreach my $joiner (@joiner_array)
 {
 	system("rm -f state-$joiner.dat");
@@ -25,6 +25,6 @@ foreach my $joiner (@joiner_array)
 	system("grep \"$joiner:joiner\\s*input-rate\" metrics.log | sed 's/^201[0-9][-][0-9]*[-][0-9]*[ \t]*[0-9]*:[0-9]*:[0-9]*,[0-9]*[ \t]*[0-9]*[ \t]*//' | sed 's/[ \t]*astro2.cs.pitt.edu:[0-9]*[ \t]*$joiner:joiner[ \t]*input-rate[ \t]*/\t/' > input-rate-$joiner.dat");
 }
 
-#get throughput of the joiners
+# get throughput of the joiners
 system("rm -f throughput.dat");
 system("grep \":joiner\\s*throughput\" metrics.log | sed 's/^201[0-9][-][0-9]*[-][0-9]*[ \t]*[0-9]*:[0-9]*:[0-9]*,[0-9]*[ \t]*[0-9]*[ \t]*//' | sed 's/[ \t]*astro2.cs.pitt.edu:6700[ \t]*/\t/' | sed 's/:joiner[ \t]*throughput[ \t]*/\t/' > throughput.dat");
